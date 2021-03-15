b7ed213683961cefd0ded74134245725557c29f2
有馬高志 <arimatakashi@arimatakashinoMacBook-Pro.local>
2021-03-16 00:12:35 +0900

    add new file

---
+++ main.js
@@ -0,0 +1,41 @@
+let time = document.getElementById("time");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let mseconds = 0;
+let interval;
+
+function stopWatch(){
+  mseconds++;
+    if(mseconds / 10 == 1){
+      seconds++;
+      mseconds = 0;
+    }
+      if(seconds / 60 == 1){
+        minutes++;
+        seconds = 0;
+      }
+        if(minutes / 60 == 1){
+          hours++;
+          minutes = 0;
+        }
+        
+time.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + mseconds;
+}
+
+start.addEventListener("click",function(){
+  interval = setInterval(stopWatch, 100);
+})
+
+stop.addEventListener("click",function(){
+  clearInterval(interval);
+})
+
+reset.addEventListener("click",function(){
+  clearInterval(interval);
+  time.innerHTML = "0:0:0:0"
+})
